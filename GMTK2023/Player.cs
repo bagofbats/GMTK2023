c71b2cc854383660fce1fbd156ac4cb5c49170ac
bagofbats <colincrowe0@gmail.com>
2023-07-06 15:29:11 -0400

    test commit

---
+++ GMTK2023/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMTK2023
+{
+    public class Player
+    {
+        // this is an empty class to make sure i set up the github repo properly
+    }
+}
