766c9d0eff349c468f92b3ff44878adc4d1c2f9f
bagofbats <colincrowe0@gmail.com>
2023-07-07 14:45:36 -0400

    basic graphics setup stuff

--- GMTK2023/GMTK2023.cs
+++ GMTK2023/GMTK2023.cs
@@ -9,11 +9,40 @@ namespace GMTK2023
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Rectangle _screenRectangle;
+        private RenderTarget2D _nativeRenderTarget;
+
         public GMTK2023()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            // determine how much to scale the window up
+            // given how big the monitor is
+            int w = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            int h = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+            int target_w = 320;
+            int target_h = 240;
+            int scale = 1;
+
+            while (target_w * (scale + 1) < w && target_h * (scale + 1) < h)
+                scale++;
+
+            target_w *= scale;
+            target_h *= scale;
+
+            _graphics.PreferredBackBufferWidth = target_w;  // set this value to the desired width of your window
+            _graphics.PreferredBackBufferHeight = target_h;   // set this value to the desired height of your window
+
+            _screenRectangle = new Rectangle(0, 0, target_w, target_h);
+
+            _graphics.ApplyChanges();
+
+            
+
+            Window.Title = "Shadow Twin";
         }
 
         protected override void Initialize()
@@ -21,6 +50,8 @@ namespace GMTK2023
             // TODO: Add your initialization logic here
 
             base.Initialize();
+
+            _nativeRenderTarget = new RenderTarget2D(GraphicsDevice, 320, 240);
         }
 
         protected override void LoadContent()
@@ -42,9 +73,16 @@ namespace GMTK2023
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetRenderTarget(_nativeRenderTarget);
+            GraphicsDevice.Clear(Color.DarkSlateGray);
+
+            GraphicsDevice.SetRenderTarget(null);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+            _spriteBatch.Draw(_nativeRenderTarget, _screenRectangle, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
